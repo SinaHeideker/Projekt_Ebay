8442daaed1072f2802136f87725dfded4d439c7d
Stefanie von Siegroth <Steffi1908>
2022-03-29 12:51:00 +0200

    test

--- Webprojekt/backend/routes/verkaufteArtikel.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const router = express.Router();    // Funktion => Routen in verschiedenen Dateien und in app.js importieren
-const Artikel = require('../models/Artikel');
-const User = require('../models/User');
